226c3f50fe28f20ddc04bddbb0c616f7ef512c9c
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2020-12-17 15:52:00 -0500

    Implement dashboard UI

--- admin/lib/apollo.js
+++ admin/lib/apollo.js
@@ -1,5 +1,6 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { from, ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
 import { LOCAL_STORAGE_TOKEN } from "./constants";
 import { onLogout } from "./auth";
 
@@ -13,40 +14,43 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    const isUnauthorized =
+      graphQLErrors && graphQLErrors.some((e) => e.message === "Unauthorized");
+
+    // Log out and redirect to login page if unauthorized
+    if (isUnauthorized) {
+      onLogout();
+      window.location.href = "/login";
+    }
+
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+
+  if (networkError) {
+    // TODO figure out what to do with network errors
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
 // For more information on `NEXT_PUBLIC_` prefixes on env vars,
 // see https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
 export const client = new ApolloClient({
   ssrMode: typeof window === "undefined",
-  link: authLink.concat(
+  link: from([
+    authLink,
+    errorLink,
     new HttpLink({
       uri: process.env.NEXT_PUBLIC_ODDNAAN_GRAPHQL_ENDPOINT,
-    })
-  ),
+    }),
+  ]),
   cache: new InMemoryCache(),
-  onError: ({ graphQLErrors, networkErrors }) => {
-    if (graphQLErrors) {
-      const isUnauthorized =
-        graphQLErrors &&
-        graphQLErrors.some((e) => e.message === "Unauthorized");
-
-      // Log out and redirect to login page if unauthorized
-      if (isUnauthorized) {
-        onLogout();
-        window.location.href = "/login";
-      }
-
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      );
-    }
-
-    if (networkErrors) {
-      // TODO figure out what to do with network errors
-      console.log(`[Network error]: ${networkErrors}`);
-    }
-  },
+  onError: ({ graphQLErrors, networkErrors }) => {},
 });
 
 export const publicClient = new ApolloClient({
--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,37 +1,72 @@
-import { useEffect, useState } from "react";
-import { Pane } from "evergreen-ui";
 import { useQuery } from "@apollo/client";
-import { LOCAL_STORAGE_TOKEN } from "../lib/constants";
+import { format } from "date-fns";
+import { Checkbox, Heading, majorScale, Pane, Spinner } from "evergreen-ui";
 import { ALL_ASSETS_QUERY, ALL_POSTS_QUERY } from "../lib/graphql/queries";
 // import styles from "../styles/Home.module.css";
 
-const getAuthToken = () => {
-  return window.localStorage.getItem(LOCAL_STORAGE_TOKEN);
+const PostRow = ({ post, index }) => {
+  return (
+    <Pane
+      key={post.uuid}
+      width="100%"
+      display="flex"
+      background={index % 2 !== 0 ? undefined : "tint1"}
+    >
+      <Pane
+        flexShrink={1}
+        display="flex"
+        alignItems="center"
+        paddingX={majorScale(1)}
+      >
+        {format(new Date(parseInt(post.createdAt, 10)), "p PP")}
+      </Pane>
+      <Pane
+        flex={4}
+        display="flex"
+        alignItems="center"
+        paddingX={majorScale(4)}
+      >
+        {post.title}
+      </Pane>
+      <Pane flexShrink={1} paddingX={majorScale(1)}>
+        <Checkbox checked={post.isPublished} label="published" />
+      </Pane>
+    </Pane>
+  );
 };
 
-const LoadingState = () => (
-  <Pane
-    height="100%"
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-  >
-    <h1>Loading</h1>
-  </Pane>
-);
+const AssetRow = ({ asset, index }) => {
+  return (
+    <Pane
+      key={asset.uuid}
+      width="100%"
+      display="flex"
+      background={index % 2 !== 0 ? undefined : "tint1"}
+    >
+      <Pane
+        flexShrink={1}
+        display="flex"
+        alignItems="center"
+        paddingX={majorScale(1)}
+      >
+        {format(new Date(parseInt(asset.createdAt, 10)), "p PP")}
+      </Pane>
+      <Pane
+        flex={4}
+        display="flex"
+        alignItems="center"
+        paddingX={majorScale(4)}
+      >
+        <a href={asset.url}>{asset.name}</a>
+      </Pane>
+      <Pane flexShrink={1} paddingX={majorScale(1)}>
+        <Checkbox checked={asset.isPrivate} label="private" />
+      </Pane>
+    </Pane>
+  );
+};
 
 export default function Dashboard() {
-  const [isLoadingToken, setIsLoadingToken] = useState(true);
-
-  // Check for token in local storage. If it does not exist, redirect.
-  useEffect(() => {
-    if (!getAuthToken()) {
-      window.location.href = "/login";
-    } else {
-      setIsLoadingToken(false);
-    }
-  }, []);
-
   const {
     loading: allPostsLoading,
     error: allPostsError,
@@ -44,26 +79,59 @@ export default function Dashboard() {
     data: allAssetsData,
   } = useQuery(ALL_ASSETS_QUERY);
 
-  // TODO Reflect assets loading state in child component
-  if (isLoadingToken || allPostsLoading) {
-    return <LoadingState />;
-  }
+  const { posts = [] } = allPostsData || {};
+  const { assets = [] } = allAssetsData || {};
 
   return (
-    <div>
-      <h1>wooo boy</h1>
-      <h2>posts</h2>
-      <ul>
-        {allPostsData.map((p) => (
-          <li>{p.title}</li>
-        ))}
-      </ul>
-      <h2>assets</h2>
-      <ul>
-        {allAssetsData.map((a) => (
-          <li>{a.name}</li>
-        ))}
-      </ul>
-    </div>
+    <Pane
+      padding={majorScale(4)}
+      height="100%"
+      display="flex"
+      flexDirection="column"
+    >
+      <Pane flexShrink={1}>
+        <Heading size={900}>oddnaan.com</Heading>
+      </Pane>
+
+      <Pane flex={1} display="flex" flexDirection="column" minHeight={0}>
+        <Heading flexShrink={1} size={700} marginY={majorScale(2)}>
+          posts
+        </Heading>
+        <Pane
+          flex={1}
+          elevation={2}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          overflowY="scroll"
+        >
+          {allPostsLoading ? (
+            <Spinner />
+          ) : (
+            posts.map((p, i) => <PostRow post={p} index={i} />)
+          )}
+        </Pane>
+      </Pane>
+
+      <Pane flex={1} display="flex" flexDirection="column">
+        <Heading flexShrink={1} size={700} marginY={majorScale(2)}>
+          assets
+        </Heading>
+        <Pane
+          flex={1}
+          elevation={2}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          overflowY="scroll"
+        >
+          {allAssetsLoading ? (
+            <Spinner />
+          ) : (
+            assets.map((a, i) => <AssetRow asset={a} index={i} />)
+          )}
+        </Pane>
+      </Pane>
+    </Pane>
   );
 }
