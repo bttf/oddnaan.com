ed2e91fc94025e3f998ba0acd5008253ab099c19
bttf <adnanchowdhury88@gmail.com>
2021-03-06 20:25:08 -0500

    Enable migrations; Add post migration

---
+++ api/src/data/migrations/1615078267832-post_add_slug_column.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class postAddSlugColumn1615078267832 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE post
+        ADD COLUMN slug TEXT CONSTRAINT alphanumeric CHECK (slug ~* '^[a-zA-Z0-9][a-zA-Z0-9_]+[a-zA-Z0-9]$');
+      `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE post
+        DROP COLUMN slug;
+      `);
+  }
+}
