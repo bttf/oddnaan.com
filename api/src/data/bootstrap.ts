ba9f3156a514f76940a410ac6df08324bab08de6
bttf <adnanchowdhury88@gmail.com>
2021-03-06 19:37:11 -0500

    TypeORM: Switch to using environment variables

--- api/src/data/bootstrap.ts
+++ api/src/data/bootstrap.ts
@@ -1,28 +1,4 @@
 import "reflect-metadata";
-import { Asset } from "./models/Asset";
-import { Post } from "./models/Post";
-import { User } from "./models/User";
 import { createConnection } from "typeorm";
 
-const {
-  ODDNAAN_PGHOST: PGHOST,
-  ODDNAAN_PGPORT: PGPORT,
-  ODDNAAN_PGUSER: PGUSER,
-  ODDNAAN_PGPASSWORD: PGPASSWORD,
-  ODDNAAN_PGDATABASE: PGDATABASE,
-} = process.env;
-
-createConnection({
-  type: "postgres",
-  host: PGHOST,
-  port: parseInt(PGPORT || "3000"),
-  username: PGUSER,
-  password: PGPASSWORD,
-  database: PGDATABASE,
-  entities: [Asset, Post, User],
-  synchronize: true,
-  logging: true,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+createConnection();
